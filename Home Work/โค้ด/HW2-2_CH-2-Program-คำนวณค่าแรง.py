48d9c5487cc286af39231524a5616f6cccde22a3
Pasuuu-Yahoo <pasuthorn1406@gmail.com>
2024-12-23 21:37:37 +0700

    Problem Solving

--- Home Work/โค้ด/HW2-2_CH-2-Program-คำนวณค่าแรง.py
+++ Home Work/โค้ด/HW2-2_CH-2-Program-คำนวณค่าแรง.py
@@ -1,8 +1,10 @@
-hour = int(input("Enter work hour: "))
-work_rate = int(input("Enter work rate: "))
-if hour <= 160:
-    salary = hour * work_rate
-    print(f"Salary is {salary}")
-else:
-    salary = (160 * work_rate) + ((hour - 160) * (work_rate * 1.5))
-    print(f"Salary is {salary}")
+number_worker = int(input("How many employee: "))
+for i in range(1, number_worker + 1):
+    hour = int(input("Enter work hour: "))
+    work_rate = int(input("Enter work rate: "))
+    if hour <= 160:
+        salary = hour * work_rate
+        print(f"Salary is {salary}")
+    else:
+        salary = (160 * work_rate) + ((hour - 160) * (work_rate * 1.5))
+        print(f"Salary is {salary}")
