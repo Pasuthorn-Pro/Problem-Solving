87840924cf99ecd45312a35b4e2918e51bc8c19e
Pasuthorn-Pro <pasuthorn1406@gmail.com>
2024-12-24 14:07:49 +0700

    Problem Solving

--- 24_12_24/exam_program_001.py
+++ 24_12_24/exam_program_001.py
@@ -5,11 +5,9 @@ method = input("Choose a deposit method (day/month/year): ")
 if method == "day":
     deposit_day = money / (year * 365)
     print(f"You have to deposit {deposit_day}")
-
-if method == "month":
+elif method == "month":
     deposit_month = money / (year * 12)
     print(f"You have to deposit {deposit_month}")
-
-if method == "year":
+elif method == "year":
     deposit_year = money / year
     print(f"You have to deposit {deposit_year}")
